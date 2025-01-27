9082bca476ceca025d7aebb664d45e99a91d7523
Sam Selikoff <sam.selikoff@gmail.com>
2025-01-27 15:19:01 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -83,8 +83,8 @@ export default function Home() {
   const submittedLora = LORAS.find((l) => l.model === submittedLoraModel);
 
   return (
-    <div className="flex h-full flex-col px-5">
-      <header className="flex flex-col justify-start pt-3 md:flex-row">
+    <div className="h-full">
+      {/* <header className="flex flex-col justify-start pt-3 md:flex-row">
         <div className="max-md:flex max-md:flex-col max-md:items-center max-md:text-center md:absolute md:left-1/2 md:top-6 md:-translate-x-1/2">
           <a href="https://togetherai.link" target="_blank">
             <Image alt="" className="h-8 w-auto" src={logo} />
@@ -112,21 +112,40 @@ export default function Home() {
             onChange={(e) => saveAPIKey(e.target.value)}
           />
         </div>
-      </header>
+      </header> */}
 
-      <main className="mx-auto mt-10 w-full max-w-5xl grow">
-        <form
-          className="w-full"
-          action={() => {
-            setSubmittedPrompt(prompt);
-            setSubmittedLoraModel(selectedLoraModel);
-            setSubmittedSeed(Math.floor(Math.random() * 9999999) + 1);
-          }}
-        >
-          <fieldset className="flex w-full flex-col gap-8 md:flex-row">
-            <div className="max-w-sm rounded-lg bg-gray-100 p-5">
+      <form
+        className="h-full"
+        action={() => {
+          setSubmittedPrompt(prompt);
+          setSubmittedLoraModel(selectedLoraModel);
+          setSubmittedSeed(Math.floor(Math.random() * 9999999) + 1);
+        }}
+      >
+        <fieldset className="flex h-full grow flex-col gap-8 md:flex-row">
+          <div className="max-w-sm bg-gray-100">
+            <div className="flex w-full flex-col border-b border-gray-300 p-5 max-md:mt-4">
+              <label className="text-sm">
+                <a
+                  href="https://api.together.xyz/settings/api-keys"
+                  target="_blank"
+                  className="inline-flex items-center gap-0.5 text-gray-700 transition hover:text-blue-500"
+                >
+                  Together AI API Key
+                  <ArrowUpRightIcon className="size-3" />
+                </a>{" "}
+              </label>
+              <input
+                placeholder="Enter your API key"
+                type="password"
+                value={userAPIKey}
+                className="mt-1 rounded bg-gray-200 px-3 py-2 text-xs text-gray-700 placeholder-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-300"
+                onChange={(e) => saveAPIKey(e.target.value)}
+              />
+            </div>
+
+            <div className="p-5">
               <p className="font-mono font-medium">Choose your LoRA</p>
-
               <RadioGroup.Root
                 name="lora"
                 value={selectedLoraModel}
@@ -144,7 +163,6 @@ export default function Home() {
                       required
                     >
                       <RadioGroup.Indicator className="absolute -inset-2 rounded-[6px] border-[1px] border-gray-400 bg-white shadow-sm" />
-
                       <div className="relative text-left">
                         <div className="relative">
                           <Image
@@ -171,121 +189,117 @@ export default function Home() {
                 ))}
               </RadioGroup.Root>
             </div>
+          </div>
 
-            <div className="w-full">
-              <div className="ml-auto max-w-lg">
-                <textarea
-                  name="prompt"
-                  rows={4}
-                  spellCheck={false}
-                  placeholder="Describe your image..."
-                  required
-                  value={prompt}
-                  onChange={(e) => setPrompt(e.target.value)}
-                  className="block w-full resize-none rounded-lg border border-gray-300 bg-white px-4 py-3 text-base text-gray-600 placeholder-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-300"
-                />
+          <div className="w-full">
+            <div className="ml-auto max-w-lg">
+              <textarea
+                name="prompt"
+                rows={4}
+                spellCheck={false}
+                placeholder="Describe your image..."
+                required
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                className="block w-full resize-none rounded-lg border border-gray-300 bg-white px-4 py-3 text-base text-gray-600 placeholder-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-300"
+              />
 
-                {selectedLora && (
-                  <div className="mt-4">
-                    <p className="text-xs font-bold text-gray-400">
-                      Suggestions
+              {selectedLora && (
+                <div className="mt-4">
+                  <p className="text-xs font-bold text-gray-400">Suggestions</p>
+                  <div className="mt-2 flex flex-wrap gap-2">
+                    {selectedLora.suggestions.map((suggestion, i) => (
+                      <button
+                        type="button"
+                        key={i}
+                        onClick={() => setPrompt(suggestion.prompt)}
+                        className="shrink-0 rounded-full bg-gray-100 px-2.5 py-1.5 text-sm text-gray-600 hover:bg-gray-200"
+                      >
+                        {suggestion.label}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              )}
+
+              <div className="mt-10">
+                <button
+                  disabled={isFetching}
+                  type="submit"
+                  className="inline-flex w-full items-center justify-center gap-1 rounded border border-cyan-700 bg-cyan-600 px-4 py-2 text-gray-100 shadow transition hover:bg-cyan-600/90 disabled:opacity-50"
+                >
+                  <PictureIcon className="size-4" />
+                  Generate Image
+                </button>
+              </div>
+
+              <div className="mt-20">
+                {submittedPrompt && submittedLora && submittedSeed ? (
+                  <AnimatePresence mode="wait">
+                    {isFetching ? (
+                      <motion.div
+                        key="a"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        style={{
+                          aspectRatio:
+                            submittedLora?.width && submittedLora?.height
+                              ? submittedLora.width / submittedLora.height
+                              : 4 / 3,
+                        }}
+                        className="flex h-auto max-w-full items-center justify-center gap-2 rounded-lg border border-gray-200 bg-gray-50 text-gray-500 shadow-sm"
+                      >
+                        <Spinner className="size-4" />
+                        Generating...
+                      </motion.div>
+                    ) : data ? (
+                      <motion.div
+                        key="b"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                      >
+                        <Image
+                          placeholder="blur"
+                          blurDataURL={imagePlaceholder.blurDataURL}
+                          width={submittedLora?.width ?? 1024}
+                          height={submittedLora?.height ?? 768}
+                          src={`data:image/png;base64,${data.image.b64_json}`}
+                          alt=""
+                          className={`${isFetching ? "animate-pulse" : ""} max-w-full rounded-lg border border-gray-200 object-cover shadow-sm`}
+                        />
+                        <div className="mt-2 text-center text-sm">
+                          <p className="text-gray-700">{submittedLora.name}</p>
+                          <p className="mt-1 text-gray-400">
+                            {data.prompt}{" "}
+                            <ShowCodeButton
+                              submittedPrompt={submittedPrompt}
+                              prompt={data.prompt}
+                              lora={submittedLora}
+                              seed={submittedSeed}
+                            />
+                          </p>
+                        </div>
+                      </motion.div>
+                    ) : null}
+                  </AnimatePresence>
+                ) : (
+                  <div className="flex aspect-[4/3] items-center justify-center">
+                    <p className="text-balance px-4 text-center text-gray-500">
+                      Choose from expertly crafted styles or bring your own
+                      LoRA. Enter your prompt, select a style, and bring your
+                      imagination to life.
                     </p>
-                    <div className="mt-2 flex flex-wrap gap-2">
-                      {selectedLora.suggestions.map((suggestion, i) => (
-                        <button
-                          type="button"
-                          key={i}
-                          onClick={() => setPrompt(suggestion.prompt)}
-                          className="shrink-0 rounded-full bg-gray-100 px-2.5 py-1.5 text-sm text-gray-600 hover:bg-gray-200"
-                        >
-                          {suggestion.label}
-                        </button>
-                      ))}
-                    </div>
                   </div>
                 )}
-
-                <div className="mt-10">
-                  <button
-                    disabled={isFetching}
-                    type="submit"
-                    className="inline-flex w-full items-center justify-center gap-1 rounded border border-cyan-700 bg-cyan-600 px-4 py-2 text-gray-100 shadow transition hover:bg-cyan-600/90 disabled:opacity-50"
-                  >
-                    <PictureIcon className="size-4" />
-                    Generate Image
-                  </button>
-                </div>
-
-                <div className="mt-20">
-                  {submittedPrompt && submittedLora && submittedSeed ? (
-                    <AnimatePresence mode="wait">
-                      {isFetching ? (
-                        <motion.div
-                          key="a"
-                          initial={{ opacity: 0 }}
-                          animate={{ opacity: 1 }}
-                          exit={{ opacity: 0 }}
-                          style={{
-                            aspectRatio:
-                              submittedLora?.width && submittedLora?.height
-                                ? submittedLora.width / submittedLora.height
-                                : 4 / 3,
-                          }}
-                          className="flex h-auto max-w-full items-center justify-center gap-2 rounded-lg border border-gray-200 bg-gray-50 text-gray-500 shadow-sm"
-                        >
-                          <Spinner className="size-4" />
-                          Generating...
-                        </motion.div>
-                      ) : data ? (
-                        <motion.div
-                          key="b"
-                          initial={{ opacity: 0 }}
-                          animate={{ opacity: 1 }}
-                          exit={{ opacity: 0 }}
-                        >
-                          <Image
-                            placeholder="blur"
-                            blurDataURL={imagePlaceholder.blurDataURL}
-                            width={submittedLora?.width ?? 1024}
-                            height={submittedLora?.height ?? 768}
-                            src={`data:image/png;base64,${data.image.b64_json}`}
-                            alt=""
-                            className={`${isFetching ? "animate-pulse" : ""} max-w-full rounded-lg border border-gray-200 object-cover shadow-sm`}
-                          />
-                          <div className="mt-2 text-center text-sm">
-                            <p className="text-gray-700">
-                              {submittedLora.name}
-                            </p>
-                            <p className="mt-1 text-gray-400">
-                              {data.prompt}{" "}
-                              <ShowCodeButton
-                                submittedPrompt={submittedPrompt}
-                                prompt={data.prompt}
-                                lora={submittedLora}
-                                seed={submittedSeed}
-                              />
-                            </p>
-                          </div>
-                        </motion.div>
-                      ) : null}
-                    </AnimatePresence>
-                  ) : (
-                    <div className="flex aspect-[4/3] items-center justify-center">
-                      <p className="text-balance px-4 text-center text-gray-500">
-                        Choose from expertly crafted styles or bring your own
-                        LoRA. Enter your prompt, select a style, and bring your
-                        imagination to life.
-                      </p>
-                    </div>
-                  )}
-                </div>
               </div>
             </div>
-          </fieldset>
-        </form>
-      </main>
+          </div>
+        </fieldset>
+      </form>
 
-      <footer className="mt-16 w-full items-center pb-10 text-center text-xs text-gray-600 md:mt-4 md:flex md:justify-between md:pb-5">
+      {/* <footer className="mt-16 w-full items-center pb-10 text-center text-xs text-gray-600 md:mt-4 md:flex md:justify-between md:pb-5">
         <p>
           Powered by{" "}
           <a
@@ -337,7 +351,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-      </footer>
+      </footer> */}
     </div>
   );
 }
