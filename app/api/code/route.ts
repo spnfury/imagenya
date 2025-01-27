49508763dbbc9ed0afffc5e57c3c4f62708ae661
Ryan Toronto <ryanto@gmail.com>
2025-01-27 11:49:22 -0500

    checkin wip

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -3,15 +3,17 @@ import dedent from "dedent";
 import { LORAS } from "@/data/loras";
 
 export async function POST(request: Request) {
-  let json = await request.json();
-  let lora = LORAS.find((l) => l.model === json.lora);
+  const json = await request.json();
+  const lora = LORAS.find((l) => l.model === json.lora);
 
   if (!lora) {
     return new Response("Missing lora", { status: 404 });
   }
 
-  let jsInstall = await text`npm install together-ai`;
-  let jsExample = await javascript`
+  const jsInstall = await text`npm install together-ai`;
+  const jsExample = await javascript`
+    import Together from "together-ai";
+
     const client = new Together();
     
     const response = await client.images.create({
@@ -32,26 +34,65 @@ export async function POST(request: Request) {
     console.log("Image:", response.data[0]);
   `;
 
+  const pythonInstall = await text`pip install together-ai`;
+  const pythonExample = await python`
+    from together import Together
+
+    def create_image():
+        client = Together()
+
+        response = client.images.generate(
+            prompt="ep sketch, A vibrant, colorful, sketch illustration of a sleek, retro-style sports car with bold, expressive lines, bright, pop-art inspired colors, and dynamic, swirling patterns in the background.",
+            model="black-forest-labs/FLUX.1-dev-lora",
+            height=832,
+            width=1280,
+            seed=1234,
+            steps=33,
+            image_loras=[
+                {
+                    "path": "https://huggingface.co/strangerzonehf/Flux-Sketch-Ep-LoRA",
+                    "scale": 1,
+                },
+            ],
+        )
+
+        print(response.data[0].url)
+
+    # Call the function
+    create_image()
+  `;
+
   return Response.json({
-    install: jsInstall,
-    example: jsExample,
+    js: {
+      install: jsInstall,
+      example: jsExample,
+    },
+    python: {
+      install: pythonInstall,
+      example: pythonExample,
+    },
   });
 }
 
 async function javascript(strings: TemplateStringsArray, ...values: any[]) {
-  const string = strings.reduce((result, string, i) => {
-    return result + (values[i - 1] || "") + string;
-  });
-
+  const string = extract(strings, ...values);
   return generateCode(string, "javascript");
 }
 
+async function python(strings: TemplateStringsArray, ...values: any[]) {
+  const string = extract(strings, ...values);
+  return generateCode(string, "python");
+}
+
 async function text(strings: TemplateStringsArray, ...values: any[]) {
-  const string = strings.reduce((result, string, i) => {
+  const string = extract(strings, ...values);
+  return generateCode(string, "text");
+}
+
+function extract(strings: TemplateStringsArray, ...values: any[]) {
+  return strings.reduce((result, string, i) => {
     return result + (values[i - 1] || "") + string;
   });
-
-  return generateCode(string, "text");
 }
 
 async function generateCode(code: string, lang: string) {
---
+++ examples/javascript/create-image.js
@@ -0,0 +1,21 @@
+import Together from "together-ai";
+
+const client = new Together();
+
+const response = await client.images.create({
+  prompt:
+    "ep sketch, A vibrant, colorful, sketch illustration of a sleek, retro-style sports car with bold, expressive lines, bright, pop-art inspired colors, and dynamic, swirling patterns in the background.",
+  model: "black-forest-labs/FLUX.1-dev-lora",
+  height: 832,
+  width: 1280,
+  seed: 1234,
+  steps: 33,
+  image_loras: [
+    {
+      path: "https://huggingface.co/strangerzonehf/Flux-Sketch-Ep-LoRA",
+      scale: 1,
+    },
+  ],
+});
+
+console.log(response.data[0].url);
